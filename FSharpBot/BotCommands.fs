3363a0e3c06e88a4497accdae9d5211e32995449
matthewahiles <matthewahiles@gmail.com>
2018-07-14 01:50:59 -0500

    Revert "Revert "switched over to DSharpPlus and reworked bot accordingly.""
    This reverts commit 0e09af5c56a45f0598e42e6833b3d4a1e43b8090.

---
+++ FSharpBot/BotCommands.fs
@@ -0,0 +1,17 @@
+﻿namespace FSharpBot
+
+open System.Threading.Tasks
+open DSharpPlus
+open DSharpPlus.CommandsNext
+open DSharpPlus.CommandsNext.Attributes
+open DSharpPlus.Entities
+open System
+
+type BotCommands() =
+    [<Command("hi")>]
+    member public self.hi(ctx:CommandContext) = 
+        async { ctx.RespondAsync "Hi there" |> ignore } |> Async.StartAsTask :> Task       
+
+    [<Command("echo")>]
+    member public self.echo(ctx:CommandContext) (message:string) =
+        async { ctx.RespondAsync message |> ignore } |> Async.StartAsTask :> Task 
--- FSharpBot/Examples/DiscordNet-WithConfig.fs
+++ FSharpBot/Examples/DiscordNet-WithConfig.fs
@@ -1,23 +1,36 @@
 ﻿namespace FSharpBot.Examples
-module SimpleBot =
+module DiscordNet_WithConfig =
 
     open System
     open Discord
     open Discord.WebSocket
     open System.Threading.Tasks
+    open Microsoft.Extensions.Configuration
+    open System.IO
 
     let logEvent (message:LogMessage) =
             Console.WriteLine(message.ToString())
     let logMessage (message: SocketMessage) =
             Console.WriteLine(message.Content.ToString())
+    let getConfig =
+        let builder = new ConfigurationBuilder()
+        do builder.SetBasePath( Directory.GetCurrentDirectory() ) |> ignore
+        do builder.AddJsonFile("config.json") |> ignore
+        builder.Build()
+
+    let private client = new DiscordSocketClient()
+    let private config = getConfig
+
+    let registerEvents =
+        async {
+            client.add_Log(fun message -> async {logEvent message} |> Async.StartAsTask :> _)
+            client.add_MessageReceived(fun message -> async {logMessage message} |> Async.StartAsTask :> _)
+        }
 
     let mainTask =
         async {
-            let client = new DiscordSocketClient();
-            client.add_Log(fun message -> async {logEvent message} |> Async.StartAsTask :> _)
-            client.add_MessageReceived(fun message -> async {logMessage message} |> Async.StartAsTask :> _)
-            let token = "your-token-here"
-            do! Async.AwaitTask(client.LoginAsync(TokenType.Bot, token))
+            do! registerEvents
+            do! Async.AwaitTask(client.LoginAsync(TokenType.Bot, config.["BotToken"]))
             do! Async.AwaitTask(client.StartAsync())
             do! Async.AwaitTask(Task.Delay(-1))
-    }
+        }
