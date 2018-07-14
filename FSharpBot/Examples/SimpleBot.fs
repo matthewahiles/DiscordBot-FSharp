e46eb177c8b6fda5508ef1a10a7a4ad48a61f696
matthewahiles <matthewahiles@gmail.com>
2018-07-13 21:05:10 -0500

    moved simple bot into an examples folder. Updated main page to use ConfigurationBuilder

---
+++ FSharpBot/Examples/SimpleBot.fs
@@ -0,0 +1,22 @@
+﻿module FSharpBot.Examples
+
+open System
+open Discord
+open Discord.WebSocket
+open System.Threading.Tasks
+
+let logEvent (message:LogMessage) =
+        Console.WriteLine(message.ToString())
+let logMessage (message: SocketMessage) =
+        Console.WriteLine(message.Content.ToString())
+
+let mainTask =
+    async {
+        let client = new DiscordSocketClient();
+        client.add_Log(fun message -> async {logEvent message} |> Async.StartAsTask :> _)
+        client.add_MessageReceived(fun message -> async {logMessage message} |> Async.StartAsTask :> _)
+        let token = "NDY1NzY5OTMyNDAwMDk5MzI5.DipcxQ.FYI4zQTNOUraH9vtHIeU4HQ-vuI"
+        do! Async.AwaitTask(client.LoginAsync(TokenType.Bot, token))
+        do! Async.AwaitTask(client.StartAsync())
+        do! Async.AwaitTask(Task.Delay(-1))
+    }
--- FSharpBot/Program.fs
+++ FSharpBot/Program.fs
@@ -2,19 +2,32 @@
 open Discord
 open Discord.WebSocket
 open System.Threading.Tasks
+open Microsoft.Extensions.Configuration
+open System.IO
 
 let logEvent (message:LogMessage) =
         Console.WriteLine(message.ToString())
 let logMessage (message: SocketMessage) =
         Console.WriteLine(message.Content.ToString())
+let getConfig =
+    let builder = new ConfigurationBuilder()
+    do builder.SetBasePath( Directory.GetCurrentDirectory() ) |> ignore
+    do builder.AddJsonFile("config.json") |> ignore
+    builder.Build()
+
+let private client = new DiscordSocketClient()
+let private config = getConfig
+
+let registerEvents =
+    async {
+        client.add_Log(fun message -> async {logEvent message} |> Async.StartAsTask :> _)
+        client.add_MessageReceived(fun message -> async {logMessage message} |> Async.StartAsTask :> _)
+    }
 
 let mainTask =
     async {
-        let client = new DiscordSocketClient();
-        client.add_Log(fun message -> async {logEvent message} |> Async.StartAsTask :> _)
-        client.add_MessageReceived(fun message -> async {logMessage message} |> Async.StartAsTask :> _)
-        let token = "NDY1NzY5OTMyNDAwMDk5MzI5.DinAHQ.1mPtI_rV9bHRswqZZxxLYYe6RKI"
-        do! Async.AwaitTask(client.LoginAsync(TokenType.Bot, token))
+        do! registerEvents
+        do! Async.AwaitTask(client.LoginAsync(TokenType.Bot, config.["BotToken"]))
         do! Async.AwaitTask(client.StartAsync())
         do! Async.AwaitTask(Task.Delay(-1))
     }
